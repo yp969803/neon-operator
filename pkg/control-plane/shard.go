9c237c71d91177f5a70eebee6cc6cb6888c50e3b
Yash Patel <yp969803@gmail.com>
2026-02-11 17:15:32 +0530

    Refactor createOrUpdateShard function to generate shard names dynamically based on tenant specifications

--- pkg/control-plane/shard.go
+++ pkg/control-plane/shard.go
@@ -2,9 +2,26 @@ package controlplane
 
 import (
 	"context"
+	"fmt"
+
 	corev1alpha1 "github.com/stateless-pg/stateless-pg/pkg/api/v1alpha1"
 )
 
-func (o *Operator) createOrUpdateShard(ctx context.Context, tenant *corev1alpha1.Tenant, shardName string) error {
-
+func (o *Operator) createOrUpdateShard(ctx context.Context, tenant *corev1alpha1.Tenant) error {
+    for _, cnt := range tenant.Spec.ShardParameters.Count {
+		tenantId := fmt.Sprintf("%s-%s-%s-%s", tenant.Spec.NeonClusterRef.Namespace,tenant.Spec.NeonClusterRef.Name, tenant.Namespace, tenant.Name )
+		shardName := fmt.Sprintf("%s-%d", tenantId, cnt)
+		shard := &corev1alpha1.TenantShard{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      shardName,
+				Namespace: tenant.Namespace,
+				Labels: map[string]string{
+					"tenant": tenant.Name,
+				},
+			},
+			Spec: corev1alpha1.TenantShardSpec{
+				
+			},
+		}
+	}
 }
